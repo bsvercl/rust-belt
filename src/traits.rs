e68f455c4c104d4d5762c05c03023fa4204b394a
Brad Svercl <bradsvercl@gmail.com>
2017-05-24 16:28:52 -0500

    Move static from function to module level

--- src/traits.rs
+++ src/traits.rs
@@ -1,6 +1,11 @@
 use glutin_window::GlutinWindow;
 use piston::event_loop::{EventSettings, Events};
 use piston::input::Input;
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref EVENTS: Mutex<Events> = Mutex::new(Events::new(EventSettings::default()));
+}
 
 pub trait GlutinWindowExt {
     fn next(&mut self) -> Option<Input>;
@@ -8,10 +13,6 @@ pub trait GlutinWindowExt {
 
 impl GlutinWindowExt for GlutinWindow {
     fn next(&mut self) -> Option<Input> {
-        use std::sync::Mutex;
-        lazy_static! {
-            static ref EVENTS: Mutex<Events> = Mutex::new(Events::new(EventSettings::default()));
-        }
         EVENTS.lock().unwrap().next(self)
     }
 }
